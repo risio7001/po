5f597f84492a9334aa93594164496d7b10b6b089
DESKTOP-JDKK7JJ\sun <dtdk7001@gmail.com>
2021-02-20 19:50:33 +0900

    Parcelable Upload

---
+++ app/src/main/java/com/example/parcelableex/SimpleData.java
@@ -0,0 +1,33 @@
+package com.example.parcelableex;
+
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class SimpleData implements Parcelable {
+    int number;
+    String message;
+    public SimpleData(int num, String msg){
+        number = num;
+        message = msg;
+    }
+    public SimpleData(Parcel src){
+        number = src.readInt();
+        message = src.readString();
+    }
+    public static final Creator CREATOR = new Creator() {
+        public SimpleData createFromParcel(Parcel in){
+            return new SimpleData(in);
+        }
+        public SimpleData[] newArray(int size){
+            return new SimpleData[size];
+        }
+    };
+    public void writeToParcel(Parcel dest, int flags){
+        dest.writeInt(number);
+        dest.writeString(message);
+    }
+    public int describeContents(){
+        return 0;
+    }
+}
